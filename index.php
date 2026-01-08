22044682ffdf9ecd2c9a8df058c994a2a50e473b
andreanputraarya <168192819+andreanbadeh@users.noreply.github.com>
2026-01-08 20:44:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,16 @@ session_start();
 
 include "template/header.php";
 
-// Default page
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 $path = explode('/', $page);
 
-// Halaman yang butuh login
 $need_login = ['home', 'about', 'kontak', 'profil', 'user', 'list', 'tambah', 'ubah', 'hapus'];
 
-// Jika belum login dan mengakses page private
 if (!isset($_SESSION['is_login']) && in_array($path[0], $need_login)) {
     header("Location: index.php?page=login");
     exit;
 }
 
-// Routing folder user
 if ($path[0] == "user") {
     $file = "user/" . ($path[1] ?? 'list') . ".php";
 
@@ -27,7 +23,6 @@ if ($path[0] == "user") {
     }
 
 } else {
-    // Routing folder pages
     $file = "pages/" . $path[0] . ".php";
     if (file_exists($file)) {
         include $file;
@@ -36,7 +31,6 @@ if ($path[0] == "user") {
     }
 }
 
-// Footer hanya tampil kalau bukan login
 if ($path[0] != 'login') {
     include "template/footer.php";
 }
