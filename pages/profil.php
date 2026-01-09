8852e5cf36ebde5337e2c8413615cbce81baa2fd
andreanputraarya <168192819+andreanbadeh@users.noreply.github.com>
2026-01-09 19:53:20 +0700

    Update profil.php

--- pages/profil.php
+++ pages/profil.php
@@ -5,8 +5,15 @@ if (!isset($_SESSION['is_login'])) {
     header("Location: index.php?page=login");
     exit;
 }
+
+$user = $_SESSION['user'];
 ?>
 
-<h2>Profil User</h2>
-<p>Nama: <?= $_SESSION['user']['nama']; ?></p>
-<p>Email: <?= $_SESSION['user']['email']; ?></p>
+<h2>Profil <?= ucfirst($user['role']); ?></h2>
+
+<p>Nama: <?= $user['nama']; ?></p>
+<p>Email: <?= $user['email']; ?></p>
+<p>No Telepon: <?= $user['no_telepon'] ?? '-'; ?></p>
+<p>NIM: <?= $user['nim'] ?? '-'; ?></p>
+<p>Prodi: <?= $user['prodi'] ?? '-'; ?></p>
+<p>Alamat: <?= $user['alamat'] ?? '-'; ?></p>
