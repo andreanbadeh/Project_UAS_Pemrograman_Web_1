e5cfdc61e586592bc0fe9dc8dd508aff5f7ce9ed
andreanputraarya <168192819+andreanbadeh@users.noreply.github.com>
2026-01-09 19:31:53 +0700

    Delete user directory

--- user/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "class/Database.php";
-$db = new Database();
-
-$id = $_GET['id'];
-$db->delete("data_barang", "id_barang='$id'");
-
-header("Location: index.php?page=user/list");
-?>
